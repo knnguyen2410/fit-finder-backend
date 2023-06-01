e460310c9a1e433763abf04c0c95eb94a7a5b9c8
kimnguyen <knnguyen2410@gmail.com>
2023-05-31 19:37:36 -0500

    initial commit

---
+++ src/main/java/com/example/fitfinder/FitFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fitfinder/FitFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fitfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
