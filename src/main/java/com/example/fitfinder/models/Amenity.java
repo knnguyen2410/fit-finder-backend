ae8c71608a2e283943948a51faf9eecc4c34a6ac
kimnguyen <knnguyen2410@gmail.com>
2023-06-01 10:45:16 -0500

    create amenity model

---
+++ src/main/java/com/example/fitfinder/models/Amenity.java
@@ -0,0 +1,89 @@
+package com.example.fitfinder.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name ="amenities") // name of table in H2 database
+public class Amenity {
+
+    @Column
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // primary key
+    private Long id;
+
+    @Column
+    private String category;
+
+    @Column
+    private String subcategory;
+
+    @Column
+    private String name;
+
+    @Column
+    private String details;
+
+    // no-args constructor
+    public Amenity() {}
+
+    // parameterized constructor
+    public Amenity(Long id, String category, String subcategory, String name, String details) {
+        this.id = id;
+        this.category = category;
+        this.subcategory = subcategory;
+        this.name = name;
+        this.details = details;
+    }
+
+    // getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getSubcategory() {
+        return subcategory;
+    }
+
+    public void setSubcategory(String subcategory) {
+        this.subcategory = subcategory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    @Override
+    public String toString() {
+        return "Amenity{" +
+                "id=" + id +
+                ", category='" + category + '\'' +
+                ", subcategory='" + subcategory + '\'' +
+                ", name='" + name + '\'' +
+                ", details='" + details + '\'' +
+                '}';
+    }
+}
